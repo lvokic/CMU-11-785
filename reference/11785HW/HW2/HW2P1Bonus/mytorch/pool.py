1d277a2284e2aa5ef8ddccf944c5f7f0bb4523df
lvokic <2792779840@qq.com>
2026-08-05 23:31:01 -0400

    HW2P1 Bonus

--- reference/11785HW/HW2/HW2P1Bonus/mytorch/pool.py
+++ reference/11785HW/HW2/HW2P1Bonus/mytorch/pool.py
@@ -15,10 +15,42 @@ class MaxPoolLayer:
         return self.forward(x)
 
     def forward(self, x: np.ndarray):
-        raise NotImplemented
+        batch_size, in_channel, self.in_w, self.in_h = x.shape
+        out_w = (self.in_w - self.kernel) // self.stride + 1
+        out_h = (self.in_h - self.kernel) // self.stride + 1
+        out = np.zeros((batch_size, in_channel, out_w, out_h))
+        self.used = np.zeros(
+            (batch_size, in_channel, out_w, out_h, self.kernel, self.kernel),
+            dtype=bool,
+        )
+        for b in range(batch_size):
+            for ic in range(in_channel):
+                for i in range(out_w):
+                    for j in range(out_h):
+                        start_w = i * self.stride
+                        end_w = start_w + self.kernel
+                        start_h = j * self.stride
+                        end_h = start_h + self.kernel
+                        window = x[b, ic, start_w:end_w, start_h:end_h]
+                        max_value = np.max(window)
+                        out[b, ic, i, j] = max_value
+                        self.used[b, ic, i, j] = window == max_value
+        return out
 
     def backward(self, delta):
-        raise NotImplemented
+        batch_size, channel, out_w, out_h = delta.shape
+        dx = np.zeros((batch_size, channel, self.in_w, self.in_h))  # type: ignore
+        for b in range(batch_size):
+            for c in range(channel):
+                for i in range(out_w):
+                    for j in range(out_h):
+                        start_w = i * self.stride
+                        start_h = j * self.stride
+                        end_w = start_w + self.kernel
+                        end_h = start_h + self.kernel
+                        mask = self.used[b, c, i, j]  # type: ignore
+                        dx[b, c, start_w:end_w, start_h:end_h][mask] = delta[b, c, i, j]
+        return dx
 
 
 class MeanPoolLayer:
@@ -34,7 +66,36 @@ class MeanPoolLayer:
         return self.forward(x)
 
     def forward(self, x):
-        raise NotImplemented
+        batch_size, in_channel, self.in_w, self.in_h = x.shape
+        out_w = (self.in_w - self.kernel) // self.stride + 1
+        out_h = (self.in_h - self.kernel) // self.stride + 1
+        out = np.zeros((batch_size, in_channel, out_w, out_h))
+        self.used = np.zeros_like(x)
+        for b in range(batch_size):
+            for ic in range(in_channel):
+                for i in range(out_w):
+                    for j in range(out_h):
+                        start_w = i * self.stride
+                        end_w = start_w + self.kernel
+                        start_h = j * self.stride
+                        end_h = start_h + self.kernel
+                        window = x[b, ic, start_w:end_w, start_h:end_h]
+                        mean_value = np.mean(window)
+                        out[b, ic, i, j] = mean_value
+        return out
 
     def backward(self, delta):
-        raise NotImplemented
+        batch_size, channel, out_w, out_h = delta.shape
+        dx = np.zeros((batch_size, channel, self.in_w, self.in_h))  # type: ignore
+        for b in range(batch_size):
+            for c in range(channel):
+                for i in range(out_w):
+                    for j in range(out_h):
+                        start_w = i * self.stride
+                        start_h = j * self.stride
+                        end_w = start_w + self.kernel
+                        end_h = start_h + self.kernel
+                        dx[b, c, start_w:end_w, start_h:end_h] += delta[b, c, i, j] / (
+                            self.kernel**2
+                        )
+        return dx
